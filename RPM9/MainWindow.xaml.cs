4341cec28c55c673929491886bef347b662342c4
ИСП-31 <-31@LAB17-05>
2025-11-12 15:59:52 +0300

    RPM9 - COLLECTION

--- RPM9/MainWindow.xaml.cs
+++ RPM9/MainWindow.xaml.cs
@@ -19,9 +19,71 @@ namespace RPM9
         /// <summary>
         /// Коллекцмя
         /// </summary>
+ 
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        List <int> mas = new List<int>(); 
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();   
+        }
+
+        private void btnFull_Click(object sender, RoutedEventArgs e)
+        {
+            bool f1 = int.TryParse(tbCol.Text, out int col);
+            if (f1 == true && col > 0)
+            {
+                mas.Clear();
+                Random rnd = new Random();
+                for (int i = 0; i < col; i++)
+                {
+                    int random = rnd.Next(-100, 100);
+                    mas.Add(random);
+                }
+                lbList.ItemsSource = null;
+                lbList.ItemsSource = mas;
+            }
+            else
+            {
+                MessageBox.Show("Проверьте правильность вводимых данных"); 
+            }
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            mas.Clear();
+            lbList.ItemsSource = null;
+            //lbList.Items.Clear();
+            tbCol.Clear();
+        }
+
+        private void btnCalc_Click(object sender, RoutedEventArgs e)
+        {
+            int plus = 0;
+            int minus = 0;
+            for (int i = 0; i < mas.Count; i++)
+            {
+                if (mas[i]>0 && mas[i]!=0)
+                {
+                    plus += 1;
+                }
+                if (mas[i]<0 && mas[i]!=0)
+                {
+                    minus += 1;
+                }
+                tbPlus.Text = plus.ToString();
+                tbMinus.Text = minus.ToString();
+            }
+        }
+
+        private void btnAbout_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Практическая работа №9 - Коллекции. Обобщённые типы.\r\n5. Дан массив в диапазоне [-100;100] найти количество положительных и отрицательных.\r\nВыполнила:\r\nСтудентка гр.ИСП-31\r\nКирюшова.В");
         }
     }
 }
